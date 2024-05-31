4600b36b3bd8cfb83c950e8ed54ce00e6eca12c6
shanuka <shanukachathuranga21@gmail.com>
2024-05-31 19:28:21 +0530

    gcd func finished

--- Algorithmic Toolbox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
+++ Algorithmic Toolbox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.py
@@ -1,12 +1,27 @@
-def gcd(a, b):
-    current_gcd = 1
-    for d in range(2, min(a, b) + 1):
-        if a % d == 0 and b % d == 0:
-            if d > current_gcd:
-                current_gcd = d
+# def gcd(a, b):
+#     current_gcd = 1
+#     for d in range(2, min(a, b) + 1):
+#         if a % d == 0 and b % d == 0:
+#             if d > current_gcd:
+#                 current_gcd = d
+#
+#     return current_gcd
 
-    return current_gcd
+def gcd(a,b):
+    firstNumber = a
+    secondNumber = b
 
+    while firstNumber != 0 and secondNumber != 0:
+        if firstNumber > secondNumber:
+            firstNumber = firstNumber % secondNumber
+        elif secondNumber > firstNumber:
+            secondNumber = secondNumber % firstNumber
+    if firstNumber == 0:
+        return secondNumber
+    elif secondNumber == 0:
+        return firstNumber
+    else:
+        return 0
 
 if __name__ == "__main__":
     a, b = map(int, input().split())
