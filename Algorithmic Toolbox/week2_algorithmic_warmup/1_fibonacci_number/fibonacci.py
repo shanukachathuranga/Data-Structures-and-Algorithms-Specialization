1c0c23a9e176486586fcea56a3cde20f0be4a97c
shanuka <shanukachathuranga21@gmail.com>
2024-05-31 17:51:44 +0530

    fib func finished

--- Algorithmic Toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ Algorithmic Toolbox/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,8 +1,22 @@
-def fibonacci_number(n):
-    if n <= 1:
-        return n
+# def fibonacci_number(n):
+#     if n <= 1:
+#         return n
+#
+#     return fibonacci_number(n - 1) + fibonacci_number(n - 2)
 
-    return fibonacci_number(n - 1) + fibonacci_number(n - 2)
+def fibonacci_number(n):
+    if n<= 1:
+        return n
+    else:
+        first_number = 0
+        second_number = 1
+        fib_number = 0;
+        for i in range(2, n+1):
+            fib_number = first_number + second_number
+            first_number = second_number
+            second_number = fib_number
+
+        return fib_number
 
 
 if __name__ == '__main__':
