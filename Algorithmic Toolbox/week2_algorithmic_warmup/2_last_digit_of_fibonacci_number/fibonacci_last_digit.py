d557b113666ae05680ed73bc4449ed5718e14aa1
shanuka <shanukachathuranga21@gmail.com>
2024-05-31 18:23:13 +0530

    fib last digit func finished

--- Algorithmic Toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ Algorithmic Toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -6,11 +6,12 @@ def fibonacci_last_digit(n):
     current  = 1
 
     for _ in range(n - 1):
-        previous, current = current, previous + current
+        previous, current = current%10, (previous + current)%10
 
-    return current % 10
+    return current
 
 
 if __name__ == '__main__':
     n = int(input())
     print(fibonacci_last_digit(n))
+
