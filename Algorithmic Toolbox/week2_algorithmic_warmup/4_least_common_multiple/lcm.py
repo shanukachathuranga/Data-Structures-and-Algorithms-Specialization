4328a2ecb0639df576be8304902f0b27089ef9d1
shanuka <shanukachathuranga21@gmail.com>
2024-06-01 18:51:11 +0530

    lcm and fib_huge func done

--- Algorithmic Toolbox/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ Algorithmic Toolbox/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -1,12 +1,51 @@
+import random
+
+
+def gcd(a, b):
+    firstNumber = a
+    secondNumber = b
+
+    while firstNumber != 0 and secondNumber != 0:
+        if firstNumber > secondNumber:
+            firstNumber = firstNumber % secondNumber
+        elif secondNumber > firstNumber:
+            secondNumber = secondNumber % firstNumber
+    if firstNumber == 0:
+        return secondNumber
+    elif secondNumber == 0:
+        return firstNumber
+    else:
+        return 0
+
+
+# def lcmBasic(a, b):
+#     for l in range(1, a * b + 1):
+#         if l % a == 0 and l % b == 0:
+#             return l
+#
+#     assert False
+
+
 def lcm(a, b):
-    for l in range(1, a * b + 1):
-        if l % a == 0 and l % b == 0:
-            return l
+    return (a * b) // gcd(a, b)
 
-    assert False
 
+# def test():
+#     while True:
+#         a, b = random.randint(1, 100000), random.randint(1, 100000)
+#         print(a, b)
+#         c = lcmBasic(a, b)
+#         print(c)
+#         d = lcm(b, a)
+#         if c != d:
+#             print(f"[{a},{b}] --> {c} != {d}]")
+#             break
+#
+#         print(f"[{a},{b}] --> {c} = {d}")
+#
+
+# test()
 
 if __name__ == '__main__':
     a, b = map(int, input().split())
     print(lcm(a, b))
-
--- Algorithmic Toolbox/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ Algorithmic Toolbox/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -1,6 +1,20 @@
-def fibonacci_huge_naive(n, m):
+import random
+
+# def fibonacci_huge_naive(n, m):
+#     if n <= 1:
+#         return n
+#
+#     previous = 0
+#     current  = 1
+#
+#     for _ in range(n - 1):
+#         previous, current = current, previous + current
+#
+#     return current % m
+
+def fibonacci(n,m):
     if n <= 1:
-        return n
+        return n%m
 
     previous = 0
     current  = 1
@@ -10,7 +24,29 @@ def fibonacci_huge_naive(n, m):
 
     return current % m
 
+def pisano_period(m):
+    previous, current = 0, 1
+    for i in range(m*m):
+        previous, current = current, (previous + current) % m
+        if previous == 0 and current == 1:
+            # print(i+1)
+            return i+1
+
+def fibonacci_huge(n, m):
+    return fibonacci((n%pisano_period(m)),m)
+
+# def test():
+#     while True:
+#         n,m = random.randint(1,100),random.randint(2,100)
+#         print(n,m)
+#         a,b = fibonacci_huge(n,m), fibonacci_huge_naive(n,m)
+#         if a != b:
+#             print(f"[{n},{m}] --> {a} != {b}")
+#             break
+#         else:
+#             print(f"[{n},{m}] --> {a} == {b}")
+# test()
 
 if __name__ == '__main__':
     n, m = map(int, input().split())
-    print(fibonacci_huge_naive(n, m))
+    print(fibonacci_huge(n, m))
