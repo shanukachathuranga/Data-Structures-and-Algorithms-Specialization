2e1384bd76f2542c7ba694ef03b51e5a7f6ea23f
shanuka chathuranga <shanukachathuranga21@gmail.com>
2024-08-19 13:38:48 +0530

    quick sort improvement

--- Algorithmic Toolbox/week4_divide_and_conquer/4_improving_quicksort/sorting.py
+++ Algorithmic Toolbox/week4_divide_and_conquer/4_improving_quicksort/sorting.py
@@ -1,8 +1,29 @@
 from random import randint
 
 
+def swap(array, val1, val2):
+    temp = array[val1]
+    array[val1] = array[val2]
+    array[val2] = temp
+
+
 def partition3(array, left, right):
-    # write your code here
+    m1 = left
+    m2 = left
+    compare_value = array[left]
+
+    for i in range(left + 1, right + 1):
+        if array[i] < compare_value:
+            m1 += 1
+            swap(array, i, m1)
+            m2 += 1
+            swap(array, m1, m2)
+        elif array[i] == compare_value:
+            m2 += 1
+            swap(array, i, m2)
+
+    swap(array, left, m1)
+    return m1, m2
 
 
 def randomized_quick_sort(array, left, right):
